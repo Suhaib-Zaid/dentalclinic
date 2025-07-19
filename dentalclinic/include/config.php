7e2e93a63b1d4048581c8027fb7e300e87cb0117
Suhaib Zaid <suhaib.mahmoud2004@gmail.com>
2025-07-19 14:34:16 +0300

    Initial commit

---
+++ dentalclinic/include/config.php
@@ -0,0 +1,12 @@
+<?php
+defined('server') ? null : define("server", "mysql.railway.internal");
+defined('user') ? null : define ("user", "root") ;
+defined('pass') ? null : define("pass","gWrTfdvpeBPQuMrQsAUhwYuctYFvXRTx");
+defined('database_name') ? null : define("database_name", "railway") ;
+ 
+// $web_root =  "https://mcguyverja.com/invoice/"; 
+$web_root ="http://localhost/dentalclinic/";
+
+
+define ('web_root' , $web_root); 
+?>
